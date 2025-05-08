118bab49fa84c4dd923679c03909d86b423053db
Jaimurugan-c <jaimurugan1703@gmail.com>
2025-05-08 12:31:59 +0530

    Done- commited

---
+++ frontend/src/components/newsletter/NewsLetter.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './newsletter.css'
+const NewsLetter = () => {
+  return (
+    <div className='newsletter'>
+       
+      <h2>Get Exclusive Offers On Your Email....</h2>
+      <p>Subscribe to our newsletter and stay update!</p>
+        <div>
+          <input type ="email" placeholder='Enter email id'/>
+          <button>Subscribe</button>
+        </div>
+    </div>
+  )
+}
+
+export default NewsLetter
