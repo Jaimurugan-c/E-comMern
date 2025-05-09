83c02d8ac85f9dff0b9fe4bda95a08eb086491eb
Jaimurugan-c <jaimurugan1703@gmail.com>
2025-05-09 11:41:58 +0530

    09/05/2025

---
+++ frontend/src/context/ShopContext.jsx
@@ -0,0 +1,14 @@
+import React, { createContext } from "react";
+import all_product from '../components/assets/all_product.js';
+
+export const ShopContext = createContext(null);
+
+export const ShopContextProvider = (props) => {
+    const contextValue = { all_product };
+
+    return (
+        <ShopContext.Provider value={contextValue}>
+            {props.children}
+        </ShopContext.Provider>
+    );
+};
