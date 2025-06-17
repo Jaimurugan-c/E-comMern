b13d23aa1db15d29343950ba53ac5b8c59b46aee
Jaimurugan-c <jaimurugan1703@gmail.com>
2025-06-17 12:18:33 +0530

    Add Navbar component and styles to App

--- admin/src/components/navbar/Navbar.jsx
+++ admin/src/components/navbar/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import './navbar.css'
+import navlogo from'../../assets/nav-logo.svg' 
+import navprofile from '../../assets/nav-profile.svg'
 const Navbar = () => {
   return (
     <div className='navbar'>
-      
+      <img src={navlogo} className='nav-logo' alt="logo" srcset="" />
+     <img src ={navprofile} className='nav-profile' />
     </div>
   )
 }
